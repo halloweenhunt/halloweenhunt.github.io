80e299a089752c555eb8ebfcf5109545052cfefd
halloweenhunt <73437941+halloweenhunt@users.noreply.github.com>
2020-10-25 21:09:37 +0000

    started adding puzzles

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 function enter_code() { 
     let input = document.getElementById('codeentry').value;
     input=input.toLowerCase();
-    if (input == "start") {
-        window.location.replace("one.html")
+    if (input == "vampire") {
+        window.location.replace("vampire.html")
+    } else if (input == "witchesbrew") {
+	window.location.replace("witchesbrew.html")
+    } else if (input == "ghost") {
+	window.location.replace("ghost.html")
+    } else if (input == "monster") {
+	window.location.replace("monster.html")
     }
+
     // window.location.replace(input.concat(".html"));
 }
 function back() {
