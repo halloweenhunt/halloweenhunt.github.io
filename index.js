71536c42f95ea4bcb612e2031ad71e8dc989f680
halloweenhunt <73437941+halloweenhunt@users.noreply.github.com>
2020-10-30 17:24:12 +0000

    finished challenges and added secret

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ function enter_code() {
         window.location.replace("vampire.html")
     } else if (input == "witchesbrew") {
 	window.location.replace("witchesbrew.html")
+    } else if (input == "witches brew") {
+	window.location.replace("witchesbrew.html")
     } else if (input == "ghost") {
 	window.location.replace("ghost.html")
     } else if (input == "monster") {
@@ -17,11 +19,37 @@ function enter_code() {
 	window.location.replace("skull.html")
     } else if (input == "spooky") {
 	window.location.replace("spooky.html")
+    } else if (input == "spookyscaryskeletons") {
+	window.location.replace("skeleton.html")
+    } else if (input == "spooky scary skeletons") {
+	window.location.replace("skeleton.html")
+    } else if (input == "graveyard") {
+	window.location.replace("graveyard.html")
+    } else if (input == "viscous hibiscus") {
+	window.location.replace("viscoushibiscus.html")
+    } else if (input == "viscoushibiscus") {
+	window.location.replace("viscoushibiscus.html")
+    } else if (input == "jackolantern") {
+	window.location.replace("finish.html")
+    } else if (input == "jack o lantern") {
+	window.location.replace("finish.html")
+    } else if (input == "jack-o-lantern") {
+	window.location.replace("finish.html")
     }
 
     // window.location.replace(input.concat(".html"))
-
 }
+
+function enter_code() { 
+    let input = document.getElementById('tricktreat').value;
+    input=input.toLowerCase();
+    if (input == "trick") {
+        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    } else if (input == "treat") {
+	window.location.replace("treat.html")
+    }
+}
+
 function back() {
     window.history.back();
 }
